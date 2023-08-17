5872c62862b893c5990713faba1a15b8b9ed7e48
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-08-17 20:04:01 +0500

    Create increasingOrder.java

---
+++ src/increasingOrder.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class increasingOrder {
+        public static void basicRecursive (int n){
+            if (n==1){
+                System.out.print(n);
+                return;
+            }
+            basicRecursive(n-1);
+            System.out.print(n + " ");
+
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Give me any single integer");
+            int n = sc.nextInt();
+
+            basicRecursive(n);
+        }
+    }
+    
