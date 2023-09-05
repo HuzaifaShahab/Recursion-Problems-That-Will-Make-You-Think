ffb1dc990abc8f46b5892133ac864fc9258c1ef3
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-09-05 22:50:10 +0500

    Create lastOccurrenceOfElement.java

---
+++ src/lastOccurrenceOfElement.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class lastOccurrenceOfElement {
+    public static int lastOccurrence (int []array , int key , int i){
+        if (i == array.length){
+            return -1 ;
+        }
+        int isFound = lastOccurrence(array,key,i+1) ;
+
+        if (isFound != -1){
+            return isFound ;
+        }
+        if (array[i] == key) {
+            return i ;
+        }
+        return -1 ;
+    }
+    public static void main(String[] args) {
+        int [] array = {1,2,3,5,6,7,5,8,9} ;
+
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Give me a key");
+            int key = sc.nextInt () ;
+
+            int occur = lastOccurrence(array,key,0) ;
+            System.out.println("Last occurrence of an element is at index : "+occur);
+    }
+}
