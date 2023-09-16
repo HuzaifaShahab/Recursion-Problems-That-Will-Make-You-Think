3bd194366db67d26ed03b22e65cdcfbea5c48e69
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-09-16 17:27:21 +0500

    Create Tiling_Problem.java

---
+++ src/Tiling_Problem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Tiling_Problem {
+
+    public static int tilingProblem(int length){
+        if (length == 0 || length == 1) return 1 ;
+
+        // Vertical Choice
+        int nm1 = tilingProblem(length-1) ;
+        // Horizontal Choice
+        int nm2 = tilingProblem(length-2) ;
+
+        return nm1 + nm2 ;
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in) ;
+        System.out.println("Give me actual length of board");
+            int length = sc.nextInt() ;
+
+            int totalWays = tilingProblem(length) ;
+        System.out.println("Total No. of ways that can be implement are : "+totalWays);
+
+    }
+}
