172d6bc9f8473d73f3efaba663d91d74defdecf0
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-09-16 23:11:12 +0500

    Create Remove_Duplicate_In_String.java

---
+++ src/Remove_Duplicate_In_String.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Remove_Duplicate_In_String {
+    public static void removeDuplicate (String string , int index , StringBuilder newString , boolean[] map){
+        // Base Case
+        if (index >= string.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = string.charAt(index) ;
+        if (map[currChar-'a'] == true){
+            removeDuplicate(string,index+1,newString,map);
+        }
+        else{
+            map[currChar-'a'] = true ;
+            removeDuplicate(string,index+1,newString.append(currChar),map);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input Any String To Remove Duplicate Characters");
+            String string = sc.nextLine() ;
+            string = string.toLowerCase() ;
+        removeDuplicate(string,0,new StringBuilder(""),new boolean[26]) ;
+
+    }
+}
